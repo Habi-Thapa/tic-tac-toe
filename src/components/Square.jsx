a308c6859028de945b13086d546173dd7e3d446b
Rubi Thapa <habi.thapa@gmail.com>
2023-10-07 22:55:35 +0545

    game completed, need to add history.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onSquareClick }) => {
   return (
     <>
       <button
         className="bg-white border border-gray-900 border-solid text-[24px] font-bold leading-[34px] h-[34px] p-0 text-center w-[34px] mr-[-1px] mt-[-1px] float-left"
-        // onClick={handleClick}
+        onClick={onSquareClick}
       >
         {value}
       </button>

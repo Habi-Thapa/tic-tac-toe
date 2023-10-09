248f0836cb4f66285ebc94f71069befbd03e5892
Rubi Thapa <habi.thapa@gmail.com>
2023-10-09 13:55:00 +0545

    history added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,22 +2,42 @@ import React, { useState } from 'react';
 import Board from './Board';
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true);
+  //   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
 
-  function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
   }
 
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
-    <div className="game">
+    <div className="flex gap-4">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*todo*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
